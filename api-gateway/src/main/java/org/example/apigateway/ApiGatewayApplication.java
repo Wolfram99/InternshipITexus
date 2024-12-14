23d0de02fd348035e14af288054e7d7566180d3b
Alexandr Mikhalenya <alexandr.mi@bk.ru>
2024-12-14 23:33:39 +0300

    fixed circuit breakers

--- api-gateway/src/main/java/org/example/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/org/example/apigateway/ApiGatewayApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@SpringBootApplication
 
+@SpringBootApplication
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
